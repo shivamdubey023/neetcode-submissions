d8dc943baec5d24de38b9c00085eb4d700f0f945
Shivam Kumar Dubey <shivam.d@brimo.in>
2026-05-22 00:03:46 +0530

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean = ""
+        rev= ""
+        for word in s:
+            if word.isalnum():
+                clean += word.lower()
+        for char in clean:
+            rev = char + rev
+
+        return rev == clean
